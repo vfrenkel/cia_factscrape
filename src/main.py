ce22578d32f57a13b59bead6a9a0286de2515489
Victor Frenkel <victor.frenkel@gmail.com>
2012-09-12 23:22:30 -0400

    initial commit, start of project.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.6
+import sys
+
+sys.path.append('../libs/beautifulsoup4-4.1.3/')
+
+def main():
+    print "hi... I'm a python program."
+
+
+if __name__=="__main__":
+    main()
